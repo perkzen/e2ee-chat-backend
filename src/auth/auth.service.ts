161b0193d9e1510379c190f6cefd308f1acaf1b7
perkzen <perko.domen@gmail.com>
2022-03-26 14:25:31 +0100

    feat: add login endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
-import { hashData } from '../utils/hash';
+import { compareHash, hashData } from '../utils/hash';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -22,7 +23,7 @@ export class AuthService {
 
     if (checkUser) {
       throw new HttpException(
-        'User with this username already in databse',
+        'User with this username already in database!',
         HttpStatus.PRECONDITION_FAILED,
       );
     }
@@ -30,5 +31,30 @@ export class AuthService {
     return await this.db.user.create({
       data: { ...user, password: hashedPassword },
     });
+  }
+
+  async login(user: LoginDto) {
+    const dbUser = await this.db.user.findFirst({
+      where: { username: user.username },
+    });
+
+    if (!dbUser) {
+      throw new HttpException(
+        'Invalid credentials!',
+        HttpStatus.PRECONDITION_FAILED,
+      );
+    }
+
+    const validatePassword = await compareHash(user.password, dbUser.password);
+    if (!validatePassword) {
+      throw new HttpException(
+        'Invalid credentials!',
+        HttpStatus.PRECONDITION_FAILED,
+      );
+    }
+    return {
+      id: dbUser.id,
+      username: dbUser.username,
+    };
   }
 }
