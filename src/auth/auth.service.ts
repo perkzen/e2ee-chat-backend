3b4735b829885708873d7054f2c4f90ed3b4c40a
perkzen <perko.domen@gmail.com>
2022-03-26 14:19:53 +0100

    feat: add register endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import { hashData } from '../utils/hash';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private db: PrismaService) {}
+
+  async register(user: RegisterDto) {
+    if (user.password1 !== user.password2) {
+      throw new HttpException(
+        "Passwords doesn't match!",
+        HttpStatus.PRECONDITION_FAILED,
+      );
+    }
+
+    const hashedPassword = await hashData(user.password1);
+    const checkUser = await this.db.user.findFirst({
+      where: { username: user.username },
+    });
+
+    if (checkUser) {
+      throw new HttpException(
+        'User with this username already in databse',
+        HttpStatus.PRECONDITION_FAILED,
+      );
+    }
+
+    return await this.db.user.create({
+      data: { ...user, password: hashedPassword },
+    });
+  }
+}
